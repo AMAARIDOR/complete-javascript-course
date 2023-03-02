f2a543fb2e2bd3f698d16d2a25546b47f5bf29e1
Amaari <arish2k3@gmail.com>
2023-03-02 20:10:46 +0530

    handle errors

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,11 +3,16 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
+// ///////////////////////////////////////
 
-const renderCountry = function (data) {
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
+
+const renderCountry = function (data, className = '') {
   const html = `
-        <article class="country">
+        <article class="country ${className}">
           <img class="country__img" src="${data.flags.png}" />
           <div class="country__data">
             <h3 class="country__name">${data.name.common}</h3>
@@ -28,34 +33,28 @@ const renderCountry = function (data) {
   countriesContainer.style.opacity = 1;
 };
 
-const getCountryAndNeighbor = function (country) {
-  // AJAX Call country 1
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
-
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-
-    // Render Country 1
-    renderCountry(data);
-
-    // Get neighbor country 2
-    const neighbor = data.borders?.[0];
-
-    if (!neighbor) return;
-
-    // AJAX Call 2
-    const request2 = new XMLHttpRequest();
-    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbor}`);
-    request2.send();
-
-    request2.addEventListener('load', function () {
-      const [data2] = JSON.parse(this.responseText);
-
-      renderCountry(data2);
+const getCountryData = function (country) {
+  // Country 1
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+      if (!neighbour) return;
+      // Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => {
+      const [dataArray] = data;
+      renderCountry(dataArray, 'neighbour');
+    })
+    .catch(err => {
+      console.error(err);
+      renderError(`Something went wrong ${err.message}`);
     });
-  });
 };
 
-getCountryAndNeighbor('russia');
+btn.addEventListener('click', function () {
+  getCountryData('canada');
+});

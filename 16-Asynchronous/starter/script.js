540f4b4de10619b18ee306b4902a62ec97dc3e1d
Amaari <arish2k3@gmail.com>
2023-03-03 13:29:25 +0530

    catch rate limit 403 error and part 1 works in challenge 1

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,16 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 //////////////////////////////////////////
+// AUTH Code 576707058067058397448x68696
+// Reverse Geocoding address https://geocode.xyz/51.50354,-0.12768?geoit=xml&auth=your_api_key
+
+fetch(
+  'https://geocode.xyz/28.65553,77.23165?geoit=json&auth=576707058067058397448x68696'
+)
+  .then(response => {
+    if (response.status === 403)
+      throw new Error(`You are being rate limited (${response.status})`);
+    return response.json();
+  })
+  .then(data => console.log(`You are in ${data.region}`))
+  .catch(error => console.error(error));

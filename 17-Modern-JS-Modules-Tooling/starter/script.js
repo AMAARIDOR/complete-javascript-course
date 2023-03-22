c65ad54021a9eb871cc90d453a7e686bc762e9f1
Amaari <arish2k3@gmail.com>
2023-03-22 10:26:45 +0530

    update

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,1 +1,16 @@
-'use strict';
+// Importing Module
+//import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+//
+//addToCart('bread', 5);
+//console.log(price, tq);
+
+console.log('Importing Module');
+
+//import * as ShoppingCart from './shoppingCart.js';
+//ShoppingCart.addToCart('bread', 5);
+//console.log(ShoppingCart.totalPrice);
+
+// import add {addToCart, totalPrice as price, tq} from './shoppingCart.js';
+
+import add from './shoppingCart.js';
+add('pizza', 2);
